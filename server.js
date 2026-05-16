e00148e16bc6cc7e20f7174ec48540e17f8daf46
m1stD3V <jennisonernie@gmail.com>
2026-05-16 02:13:15 -0700

    chore: increase socket.io ping timeout for high-latency connections

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, { 
+  cors: { origin: "*" },
+  pingTimeout: 60000, // Increase to 60 seconds
+  pingInterval: 25000 // Increase interval to 25 seconds
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
